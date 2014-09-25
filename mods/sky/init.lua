8974f39850020815b372b7ce4f4ee5fe86e5147d
Amaz <charliejordan34@gmail.com>
2014-09-25 16:12:54 +0100

    RTM will be pleased!

--- mods/sky/init.lua
+++ mods/sky/init.lua
@@ -1,10 +1,9 @@
-
--- Set time to midnight.
+--[[ Set time to midnight.
 minetest.register_on_joinplayer(function(player)
     minetest.set_timeofday(0.22)
-    minetest.setting_set("time_speed", 0)
+    minetest.set_time_speed(0)
 end)
-
+]]--
 -- Disable clouds and enable them again when player leaves the game. (Experimental)
 minetest.register_on_joinplayer(function(player)
     minetest.setting_set("enable_clouds", 0)
@@ -14,19 +13,17 @@ minetest.register_on_leaveplayer(function(player)
 end)
 
 -- Sky textures
-if minetest.setting_get("newsky") == "1" then
-	minetest.register_on_joinplayer(function(player)
-		minetest.after(0, function()
-			skytextures ={
-			"sky_pos_y.png",
-			"sky_neg_y.png",
-			"sky_pos_z.png",
-			"sky_neg_z.png",	
-			"sky_neg_x.png",
-			"sky_pos_x.png",
-			}
-
-			player:set_sky({r=0, g=0, b=0, a=0},"skybox", skytextures)
-		end)
+minetest.register_on_joinplayer(function(player)
+	minetest.after(0, function()
+		skytextures ={
+		"sky_pos_y.png",
+		"sky_neg_y.png",
+		"sky_pos_z.png",
+		"sky_neg_z.png",	
+		"sky_neg_x.png",
+		"sky_pos_x.png",
+		}
+		player:set_sky({r=0, g=0, b=0, a=0},"skybox", skytextures)
 	end)
-end
+end)
+
